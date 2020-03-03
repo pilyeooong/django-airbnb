94ff0119fd3079fdfb1d3f281f28b9d54f14464b
PilYeong <pilyeooong@gmail.com>
2020-03-03 15:48:37 +0900

    ADD: user 모델 추가

---
+++ users/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    
+    """ Custom User Admin """
+    
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birth",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            }
+        ),
+    )
