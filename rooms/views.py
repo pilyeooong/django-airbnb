7ea4aee37f71f7350bfb280247706c09a1ab0cef
PilYeong <pilyeooong@gmail.com>
2020-03-11 20:39:39 +0900

    ADD: pagination Exception 추가

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.core.paginator import EmptyPage
 from django.core.paginator import Paginator
+from django.shortcuts import redirect
+from django.shortcuts import render
+
 from .models import Room
+
 
 def all_rooms(request):
     page = request.GET.get('page', 1)
     room_list = Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms.paginator))
-    return render(request, 'rooms/home.html', context={
-        'rooms': rooms
-    })
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, 'rooms/home.html', context={
+            'page': rooms
+        })
+    except EmptyPage:
+        return redirect('/')
