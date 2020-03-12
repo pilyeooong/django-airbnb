9ed16f0b0b3882a12bee1795aabedaa3befdefe2
PilYeong <pilyeooong@gmail.com>
2020-03-12 17:01:48 +0900

    ADD: CBV를 이용한 페이지네이션

--- rooms/views.py
+++ rooms/views.py
@@ -1,19 +1,13 @@
-from django.core.paginator import EmptyPage
-from django.core.paginator import Paginator
-from django.shortcuts import redirect
-from django.shortcuts import render
+from django.views.generic import ListView
 
 from .models import Room
 
 
-def all_rooms(request):
-    page = request.GET.get('page', 1)
-    room_list = Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, 'rooms/home.html', context={
-            'page': rooms
-        })
-    except EmptyPage:
-        return redirect('/')
+class HomeView(ListView):
+    
+    """ HomeView Definition """
+
+    model = Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = 'created'
