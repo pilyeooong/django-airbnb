2cd08c03966f4d12b0e3a4f4668329823ec967a7
PilYeong <pilyeooong@gmail.com>
2020-03-16 16:37:43 +0900

    ADD: Signup Form

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import LoginView
 from .views import log_out
+from .views import SignUpView
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', log_out, name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,27 +4,65 @@ from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.shortcuts import reverse
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import FormView
 
 from .forms import LoginForm
+from .forms import SignUpForm
 
 
-class LoginView(View):
-    def get(self, request):
-        form = LoginForm(initial={'email': 'scania289@naver.com'})
-        return render(request, 'users/login.html', {'form': form})
+class LoginView(FormView):
     
-    def post(self, request):
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse('core:home'))  
-        return render(request, 'users/login.html', {'form': form})
+    template_name = 'users/login.html'
+    form_class = LoginForm
+    success_url = reverse_lazy('core:home')
+    
+    def form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+    
+# class LoginView(View):
+#     def get(self, request):
+#         form = LoginForm(initial={'email': 'scania289@naver.com'})
+#         return render(request, 'users/login.html', {'form': form})
+    
+#     def post(self, request):
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             email = form.cleaned_data.get('email')
+#             password = form.cleaned_data.get('password')
+#             user = authenticate(request, username=email, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect(reverse('core:home'))  
+#         return render(request, 'users/login.html', {'form': form})
 
 def log_out(request):
     logout(request)
     return redirect(reverse('core:home'))
+
+
+class SignUpView(FormView):
+    
+    template_name = 'users/signup.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('core:home')
+    initial = {
+        'first_name': 'Heo',
+        'last_name': 'feel',
+        'email': 'sccs@naver.com',
+    }
+    
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
